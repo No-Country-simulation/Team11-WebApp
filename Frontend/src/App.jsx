0b94ef6b4dc705a3b25dc0f69967f5dd362247d9
Morfeo <gastongomez2014@hotmail.com>
2025-10-01 17:54:23 -0300

    Punto inicial para el Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-green-500">Hello World!</p>
+    </>
+  )
+}
+
+export default App
