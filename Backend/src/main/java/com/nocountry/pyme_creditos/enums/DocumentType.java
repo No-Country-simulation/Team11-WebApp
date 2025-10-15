95775f6a98dfbcead746deb10af491293c426eab
walterLaborde <walter.laborde@hotmail.com>
2025-10-15 15:40:55 -0300

    Update DocumentType enum

--- Backend/src/main/java/com/nocountry/pyme_creditos/enums/DocumentType.java
+++ Backend/src/main/java/com/nocountry/pyme_creditos/enums/DocumentType.java
@@ -1,5 +1,11 @@
 package com.nocountry.pyme_creditos.enums;
 
 public enum DocumentType {
-
+    ID_CARD,
+    FINANCIAL_STATEMENT,
+    BALANCE_SHEET,
+    NOTARIAL_POWER,
+    TAX_RETURN,
+    ADDRESS_PROOF,
+    BANK_REFERENCE
 }
