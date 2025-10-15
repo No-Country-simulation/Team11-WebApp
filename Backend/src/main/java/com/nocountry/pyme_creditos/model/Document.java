5a77a536a8ad5f97ef53c88cc4aa27fe059323b9
walterLaborde <walter.laborde@hotmail.com>
2025-10-15 15:41:42 -0300

    Create Documents class

---
+++ Backend/src/main/java/com/nocountry/pyme_creditos/model/Document.java
@@ -0,0 +1,48 @@
+package com.nocountry.pyme_creditos.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.nocountry.pyme_creditos.enums.DocumentType;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "documents")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Document {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id_document", updatable = false, nullable = false)
+	private UUID id;
+	
+	//Relacion con CreditApplication
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "application_id", nullable = false)
+	private CreditApplication application;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "document_type", nullable = false)
+	private DocumentType documentType;
+	
+	@Column(name = "file_url", nullable = false, length = 255)
+	private String fileUrl;
+	
+	@Column(name = "document_uploaded_at", nullable = false)
+	private LocalDateTime documentUploadeddAt = LocalDateTime.now();
+	
+	@Column(name = "is_valid", nullable = false)
+	private boolean isValid = false;
+	
+	@Column(name = "document_signed_at")
+	private LocalDateTime documentSignedAt;
+	
+	@Column(name = "is_signed", nullable = false)
+	private boolean isSigned = false;
+}
