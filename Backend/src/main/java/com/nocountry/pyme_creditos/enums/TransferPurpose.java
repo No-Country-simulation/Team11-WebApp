951b54bdfab9c39dcccca38007f24a650c90cc69
walterLaborde <walter.laborde@hotmail.com>
2025-10-15 16:09:30 -0300

    Update TransferPurpose enum

--- Backend/src/main/java/com/nocountry/pyme_creditos/enums/TransferPurpose.java
+++ Backend/src/main/java/com/nocountry/pyme_creditos/enums/TransferPurpose.java
@@ -1,4 +1,11 @@
 package com.nocountry.pyme_creditos.enums;
 
 public enum TransferPurpose {
+	  INVESTMENT,
+	  SUPPLIER_PAYMENT,
+	  SALARIES,
+	  SERVICES,
+	  TAX_PAYMENT,
+	  LOAN_DISBURSEMENT,
+	  OTHER
 }
