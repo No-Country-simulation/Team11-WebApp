81918cd6d21f02381acabdc002a6f1287f28bd25
jlugod1 <jlugopachecom@gmail.com>
2025-10-20 23:44:05 -0300

    feat: segurity quite el acceso a  /api/companies/

--- Backend/src/main/java/com/nocountry/pyme_creditos/config/SecurityConfig.java
+++ Backend/src/main/java/com/nocountry/pyme_creditos/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/auth/login",
                                 "/auth/register",
-                                "/auth/operator/register", "/api/companies/",
+                                "/auth/operator/register",
                                 "/auth/.well-known/jwks.json"
                         ).permitAll()
 
