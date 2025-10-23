c36268355c55fbfb330b4c1e1bd0eefe06d9597f
walterLaborde <walter.laborde@hotmail.com>
2025-10-23 11:07:03 -0300

    update DocumentService

--- Backend/src/main/java/com/nocountry/pyme_creditos/services/DocumentService.java
+++ Backend/src/main/java/com/nocountry/pyme_creditos/services/DocumentService.java
@@ -12,13 +12,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.nocountry.pyme_creditos.dto.DocumentResponseDTO;
 import com.nocountry.pyme_creditos.enums.DocumentType;
 import com.nocountry.pyme_creditos.exceptions.BadRequestException;
 import com.nocountry.pyme_creditos.exceptions.NotFoundException;
 import com.nocountry.pyme_creditos.model.CreditApplication;
 import com.nocountry.pyme_creditos.model.Document;
+import com.nocountry.pyme_creditos.model.User;
 import com.nocountry.pyme_creditos.repository.CreditApplicationRepository;
 import com.nocountry.pyme_creditos.repository.DocumentRepository;
+import com.nocountry.pyme_creditos.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,18 +31,15 @@ public class DocumentService {
 	
 	private final DocumentRepository documentRepository;
 	private final CreditApplicationRepository applicationRepository;
-
-	private final Path rootPath;
+	private final UserRepository userRepository;
 	
-	public DocumentService(DocumentRepository documentRepository,
-						   CreditApplicationRepository applicationRepository,
-						   @Value("${file.upload-dir:uploads}") String uploadDir) {
-		this.documentRepository = documentRepository;
-		this.applicationRepository = applicationRepository;
-		this.rootPath = Paths.get(uploadDir);
-	}
 	
-	public Document uploadDocument(UUID applicationId, DocumentType type, MultipartFile file) throws IOException {
+	@Value("${file.upload-dir:uploads}")
+	private String uploadDir;
+	
+	private static final long MAX_SIZE = 5 * 1024 * 1024; //5MB
+	
+	public DocumentResponseDTO uploadDocument(UUID applicationId, DocumentType type, MultipartFile file, User user) {
 		
 		//Verifica si existe la solicitud de crédito
 		CreditApplication app = applicationRepository.findById(applicationId)
@@ -55,32 +55,57 @@ public class DocumentService {
 		}
 		
 		//Valida el tamaño (máximo 5MB)
-		if(file.getSize() > 5 * 1024 * 1024) {
+		if(file.getSize() > MAX_SIZE) {
 			throw new BadRequestException("El tamaño del archivo es muy grande (mayor a 5MG");
 		}		
 		
-		//Crea la carpeta si no existe
-		if(!Files.exists(rootPath)) {
-			Files.createDirectories(rootPath);
-		}
-
+		Path rootPath = Paths.get(uploadDir);
 		
+		//Crea la carpeta si no existe
+			try {
+				if(!Files.exists(rootPath)) {
+				Files.createDirectories(rootPath);
+				}
+			} catch (IOException e) {
+				throw new BadRequestException("Error al crear el directorio de uploads: " + e.getMessage());
+			}
+
 		//Genera nombre único
 		String uniqueName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-		
-		//Guarda el archivo
 		Path filePath = rootPath.resolve(uniqueName);
-		Files.copy(file.getInputStream(),filePath, StandardCopyOption.REPLACE_EXISTING);
+		
+		//Lo guarda
+		try {
+			Files.copy(file.getInputStream(),filePath, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			throw new BadRequestException("Error al guardar el archivo: " + e.getMessage());
+		}
 		
 		//Crea entidad Document
 		Document document = new Document();
 		document.setApplication(app);
 		document.setDocumentType(type);
 		document.setFileUrl(filePath.toString());
-		document.setDocumentUploadeddAt(LocalDateTime.now());
+		document.setDocumentUploadedAt(LocalDateTime.now());
 		
-		return documentRepository.save(document);
+		documentRepository.save(document);
 		
+		return mapToDTO(document);
+		
+	}
+	
+	private DocumentResponseDTO mapToDTO(Document document) {
+		DocumentResponseDTO dto = new DocumentResponseDTO();
+		dto.setId(document.getId());
+		dto.setApplicationId(document.getApplication().getId());
+		dto.setDocumentType(document.getDocumentType());
+		dto.setFileUrl(document.getFileUrl());
+		dto.setDocumentUploadedAt(document.getDocumentUploadedAt());
+		dto.setValid(document.isValid());
+		dto.setSigned(document.isSigned());
+		dto.setDocumentSignedAt(document.getDocumentSignedAt());
+
+		return dto;
 	}
 	
 	private boolean isValidFileType(MultipartFile file) {
