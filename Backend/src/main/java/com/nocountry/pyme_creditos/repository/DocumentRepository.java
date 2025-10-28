e457640448e755d8dd4696106d31f100e92abd69
walterLaborde <walter.laborde@hotmail.com>
2025-10-28 03:12:39 -0300

    Create DocumentRepository

--- Backend/src/main/java/com/nocountry/pyme_creditos/repository/DocumentRepository.java
+++ Backend/src/main/java/com/nocountry/pyme_creditos/repository/DocumentRepository.java
@@ -1,9 +1,10 @@
 package com.nocountry.pyme_creditos.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.nocountry.pyme_creditos.model.Document;
 
 public interface DocumentRepository extends JpaRepository<Document,UUID>{
-
+	List<Document> findByApplicationId(UUID applicationId);
 }
